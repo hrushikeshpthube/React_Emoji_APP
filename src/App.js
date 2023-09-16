28637cf0814901de44ca78410c08efd25dad3362
hrushikeshpthube <140833389+hrushikeshpthube@users.noreply.github.com>
2023-09-16 16:18:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,25 +2,92 @@ import "./styles.css";
 import React from "react";
 import { useState } from "react";
 
-var username = "demo";
+//var username = "demo";
 // username=prompt("Enter your name");
-var texcolor = "gold";
+//var texcolor = "gold";
 
 export default function App() {
-  const [likecountr, setlikecount] = useState(0);
-
+  var emojidictionary = {
+    "😊": "Smiling Face ",
+    "😔": "Sad",
+    "😲": "Surprised",
+    "🐕": "dog",
+    "💎": "Diamond",
+  };
+  /*  var [likecountr, setlikecount] = useState(0);
+//Below likehandlercount to try for like button like on  instagram
   function likehandlercount() {
     // likecountr++;
     // console.log("Liked button=", likecountr);
     var newlikecountr = likecountr + 1;
     setlikecount(newlikecountr);
   }
-  console.log("Liked button=", likecountr);
+*/
+  /* var [userinput, setinputevent] = useState("");
+  function inputhandler(event) {
+    console.log(event.target);
+    console.log(event.target.value);
+    setinputevent(event.target.value);
+  }
+  */
+  /*  console.log("Liked button=", likecountr);*/
+
+  var [meaning, setemojihandler] = useState("");
+  var emojisweknow = Object.keys(emojidictionary);
+  function clearinput() {
+    var temp = document.getElementById("inp");
+    temp.value = "";
+  }
+  function Emojihandler(event) {
+    var userinput = event.target.value;
+    var meaning = emojidictionary[userinput];
+    // console.log(event.target.value);
+    console.log(meaning);
+    if (userinput in emojidictionary) {
+      setemojihandler(emojidictionary[userinput]);
+    } else {
+      setemojihandler("We don't have this emoji in our database");
+    }
+  }
+
+  function emojiClickHandler(emoji) {
+    // console.log(emoji);
+    clearinput();
+    var meaning = emojidictionary[emoji];
+
+    setemojihandler(emojidictionary[emoji]);
+  }
+
   return (
     <div className="App">
-      <h1 style={{ backgroundColor: texcolor }}>Welcome {username}</h1>
+      {/* <h1 style={{ backgroundColor: texcolor }}>Welcome {username}</h1>
       <button onClick={likehandlercount}>Like here</button>
       {likecountr}
+      <br></br> */}
+      <h1>Welcome to Emoji Iterpreter</h1>
+      <h3>
+        Enter an emoji to know meaning of it <br></br>OR<br></br>you can click
+        on below set of emojis
+      </h3>
+      <input id="inp" onChange={Emojihandler}></input>
+      <h2>Meaning ={meaning} </h2>
+      <h3>Emojis we Know</h3>
+      {emojisweknow.map(function (emoji) {
+        {
+          {
+            return (
+              <span
+                onClick={() => emojiClickHandler(emoji)}
+                style={{ fontSize: "2rem", padding: "1rem", cursor: "pointer" }}
+                keys={emoji} //this is to avoid this warning ->Warning: Each child in a list should have a unique "key" prop.
+              >
+                {emoji}
+              </span>
+            );
+          }
+        }
+      })}
+      {/* Welcome {userinput} */}
     </div>
   );
 }
